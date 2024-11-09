76924553a1dac115cfb0a8cc98991dc28f54d623
João Vitor <joaosilvabarbosa04@gmail.com>
2024-11-09 17:41:12 -0300

    remove unsude type

--- src/app/cadastro/components/Stepper/index.tsx
+++ src/app/cadastro/components/Stepper/index.tsx
@@ -5,7 +5,6 @@ import {
     AtividadeForm,
     CondicaoForm,
     ContatoForm,
-    DadosForm,
     EnderecoForm,
     PerfilPessoalForm,
     QuadroClinicoForm,
@@ -42,7 +41,7 @@ const steps = [
 
 export default function HorizontalLinearStepper() {
     const [activeStep, setActiveStep] = React.useState(0);
-    const [dadosForm, setDadosForm] = React.useState<DadosForm>({});
+    const [dadosForm, setDadosForm] = React.useState({});
     const [filiacaoForm, setFiliacaoForm] = React.useState([]);
     const [enderecoForm, setEnderecoForm] = React.useState<EnderecoForm>({});
     const [contatoForm, setContatoForm] = React.useState<ContatoForm>({});
